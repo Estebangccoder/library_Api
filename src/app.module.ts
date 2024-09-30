299de3919e007317c4090df894033541e964baa6
estebangccoder <estebangccoder@gmail.com>
2024-09-30 11:05:51 -0500

    Create connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './book/entities/book.entity';
 
 @Module({
-  imports: [BookModule],
+  imports: [ConfigModule.forRoot({
+     envFilePath: '.env'
+  }),
+  TypeOrmModule.forRoot({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities:  [__dirname + '**/**/*.entity{.ts,.js}', Book],
+    synchronize: false,
+    ssl: {
+      rejectUnauthorized: false, // Pilas quitar en produccion
+    },
+  }),
+  
+  BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
